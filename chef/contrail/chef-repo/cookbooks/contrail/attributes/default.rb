cd99a6f8d9668b6c977602fd28499c8bb79ead45
chinhuang007 <chhuang@us.ibm.com>
2014-12-04 11:20:41 -0800

    add and update attributes for all recipes

--- chef/contrail/chef-repo/cookbooks/contrail/attributes/default.rb
+++ chef/contrail/chef-repo/cookbooks/contrail/attributes/default.rb
@@ -1,4 +1,4 @@
-default['contrail']['contrail_version'] = '1.2'
+default['contrail']['contrail_version'] = '2.0'
 #To server your own RHEL repo, set true for this attribute and create the directory structure 'operatingsystem/redhat6.5/x86_64/' under your http server's DoucmentRoot.
 default['contrail']['setup_operatingsystem_dependencies_repo'] = false
 #Ted chang: for now Hard coding the repo server ip (http://9.30.30.65:3080/) but we need to replace it with chef url(#{Chef::Config[:chef_server_url]})
@@ -9,10 +9,10 @@ default['contrail']['keystone_ip']="1.1.1.1"
 default['contrail']['region_ip']="1.1.1.2"
 default['contrail']['network_ip']="1.1.1.3"
 default['contrail']['compute_ip']="1.1.1.4"
-default['contrail']['compute_node_interface']="eth1"
-#default['contrail']['region_name']="#{node['openstack']['region']}"
-#default['contrail']['admin_token']="#{node['openstack']['identity']['simple_token_secret']}"
+#default['contrail']['compute_node_interface']="eth1"
+#default['contrail']['single_signon_domain']="abc.xyz.com'
 
+# Contrail
 default['contrail']['openstack_release'] = "icehouse"
 default['contrail']['multi_tenancy'] = false
 default['contrail']['manage_neutron'] = false
@@ -21,10 +21,10 @@ default['contrail']['router_asn'] = 64512
 default['contrail']['service_token'] = "#{node['openstack']['identity']['simple_token_secret']}"
 default['contrail']['admin_token'] = "#{node['openstack']['identity']['simple_token_secret']}"
 default['contrail']['admin_password'] = "passw0rd"
-default['contrail']['admin_user'] = "admin"
-default['contrail']['admin_tenant_name'] = "admin"
+default['contrail']['admin_user'] = "#{node['openstack']['identity']['admin_user']}"
+default['contrail']['admin_tenant_name'] = "#{node['openstack']['identity']['admin_tenant_name']}"
 default['contrail']['haproxy'] = false
-default['contrail']['cfgm']['hostname'] = "#{node['contrail']['network_fqdn']}"
+default['contrail']['cfgm']['hostname'] = "hostname.ibm.com"
 default['contrail']['cfgm']['ip'] = "#{node['contrail']['network_ip']}"
 default['contrail']['region_name'] = "#{node['openstack']['region']}" 
 default['contrail']['openstack']['ip'] = "#{node['contrail']['region_ip']}"
@@ -38,12 +38,12 @@ default['contrail']['control']['ip'] = "#{node['contrail']['network_ip']}"
 # Analytics
 default['contrail']['analytics']['ip'] = "#{node['contrail']['network_ip']}"
 # Compute
-default['contrail']['compute']['interface'] = "#{node['contrail']['compute_node_interface']}"
+default['contrail']['compute']['interface'] = "eth1"
 default['contrail']['compute']['ip'] = "#{node['contrail']['compute_ip']}"
-default['contrail']['compute']['netmask'] = "255.255.255.0"
+default['contrail']['compute']['netmask'] = "255.255.255.128"
 default['contrail']['compute']['gateway'] = "9.30.30.1"
 default['contrail']['compute']['cidr'] = "9.30.30.0/24"
 default['contrail']['compute']['dns1'] = "9.30.140.13"
 default['contrail']['compute']['dns2'] = "9.0.9.1"
 default['contrail']['compute']['dns3'] = ""
-default['contrail']['compute']['domain'] = "hipods.svl.ibm.com"
+default['contrail']['compute']['domain'] = "aaa.bbb.com"
