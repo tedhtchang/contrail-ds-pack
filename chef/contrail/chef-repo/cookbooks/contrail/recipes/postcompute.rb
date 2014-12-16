af31821a0e8ead1c7e40f3041db384d16c8e9334
chinhuang007 <chhuang@us.ibm.com>
2014-12-15 18:43:23 -0800

    add insert lines to nova.conf for compute

--- chef/contrail/chef-repo/cookbooks/contrail/recipes/postcompute.rb
+++ chef/contrail/chef-repo/cookbooks/contrail/recipes/postcompute.rb
@@ -51,6 +51,20 @@ bash "update-nova" do
     not_if "grep -q '# vif_driver=nova.virt.libvirt.vif.LibvirtGenericVIFDriver' /etc/nova/nova.conf"
 end
 
+search_line="compute_monitors = ComputeDriverCPUMonitor"
+insert_line1="libvirt_vif_driver = nova_contrail_vif.contrailvif.VRouterVIFDriver"
+insert_line2="neutron_connection_host = #{node['contrail']['network_ip']}"
+
+ruby_block "insert-nova" do
+    block do
+		file = Chef::Util::FileEdit.new('/etc/nova/nova.conf')
+		file.insert_line_after_match(/#{search_line}/, insert_line1)
+		file.insert_line_after_match(/#{insert_line1}/, insert_line2)
+		file.write_file
+	end
+    not_if "grep -q 'libvirt_vif_driver = nova_contrail_vif.contrailvif.VRouterVIFDriver' /etc/nova/nova.conf"
+end
+
 bash "restart-libvirtd" do
     user "root"
     code <<-EOH
