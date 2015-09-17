abd0b47e4d3fd08b7dabcf5d5c27e2b40fbcf274
Chin Huang <chhuang@us.ibm.com>
2015-09-17 15:21:42 -0700

    This is test

--- chef/contrail/chef-repo/cookbooks/contrail/attributes/default.rb
+++ chef/contrail/chef-repo/cookbooks/contrail/attributes/default.rb
@@ -1,4 +1,5 @@
 require 'uri'
+# This is a test
 #To server your own RHEL repo, set true for this attribute and create the directory structure 'operatingsystem/redhat6.5/x86_64/' under your http server's DoucmentRoot.
 default['contrail']['setup_operatingsystem_dependencies_repo'] = "false"
 #Ted chang: for now Hard coding the repo server ip (http://9.30.30.65:3080/) but we need to replace it with chef url(#{Chef::Config[:chef_server_url]})
